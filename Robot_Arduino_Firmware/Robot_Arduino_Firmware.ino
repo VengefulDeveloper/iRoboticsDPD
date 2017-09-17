a2e963674a988b1a447197348521ba078fa83b12
VengefulDeveloper <el114179@gmail.com>
2017-09-16 22:59:01 -0500

    Motor Mixer Added
    The mixer for each motor's speed given the turn and forward/backward value was added, along with the averaging filter for the receiver input.
    Fixed a division by zero error when the program starts.
    Removed basic deadband code to be replaced and updated.

--- Robot_Arduino_Firmware/Robot_Arduino_Firmware.ino
+++ Robot_Arduino_Firmware/Robot_Arduino_Firmware.ino
@@ -4,10 +4,13 @@
 
 #define ChannelsUsed 4
 #define iterations 3
-unsigned long int a,b,c;
-int x[15],ch1[15],ch[iterations][6],i;
+unsigned long int a, b, c;
+int x[15], ch1[15], ch[iterations][ChannelsUsed], i;
 
-int motorValues[2] = {0,0};
+int avg[ChannelsUsed];
+
+int MixedSpeeds[2] = {0, 0};
+int motorValues[2] = {1, 1};
 
 #define weaponESC 9
 
@@ -18,47 +21,105 @@ int motorValues[2] = {0,0};
 
 #define ReceiverPin 3
 
-#define motorDeadband 0.05 //percent error between commanded and set motor pwm value
+#define motorDeadband 0.15 //percent error between commanded and set motor pwm value
 #define Motor1Multiplier 1  //change to -1 to reverse direction
 #define Motor2Multiplier 1
 
+#define redLED A0
+#define blueLED A5
+
 Servo ServoWeaponESC;
 
+
 void setup() {
+  Serial.begin(115200);
   pinMode(Motor1Dir0, OUTPUT);
   pinMode(Motor1Dir1, OUTPUT);
   pinMode(Motor2Dir0, OUTPUT);
   pinMode(Motor2Dir1, OUTPUT);
-  
+
   pinMode(ReceiverPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(ReceiverPin), read_me, FALLING);
-  
+
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+
+  pinMode(A0, OUTPUT);
+  pinMode(A5, OUTPUT);
+
   ServoWeaponESC.attach(weaponESC);
 }
 
 void loop() {
   read_rc();
-  ServoWeaponESC.write(map(ch[1],0,1000,0,180));
-  
-  if ((motorDeadband < (abs((ch[0] - motorValues[0]))/ motorValues[0])) || (motorDeadband < (abs((ch[3] - motorValues[1]))/ motorValues[1]))) {
-    updateMotors();
+  if (ch[iterations-1][3] > 700) {
+    digitalWrite(blueLED, HIGH);
+    digitalWrite(redLED, LOW);
+    ServoWeaponESC.write(map(ch[iterations][1], 0, 1000, 0, 180));
+
+    //ARMED
+  } else {
+    //NO RADIO CONNECTED
+    digitalWrite(blueLED, LOW);
+    digitalWrite(redLED, HIGH);
+  }
+}
+
+void updateMotors(int M1, int M2) {
+  motorValues[0] = M1;
+  motorValues[1] = M2;
+  int linearValue = map(M1, 0, 1000, -255, 255);
+  int rotationalValue = map(M2, 0, 1000, -255, 255);
+
+  motorMixer(linearValue, rotationalValue);
+
+  if (MixedSpeeds[0] > 0) {
+    digitalWrite(Motor1Dir1, LOW);
+    delay(1);
+    analogWrite(Motor1Dir0, MixedSpeeds[0]);
+  } else {
+    digitalWrite(Motor1Dir0, LOW);
+    delay(1);
+    analogWrite(Motor1Dir1, MixedSpeeds[0]);
+  }
+  if (MixedSpeeds[1] > 0) {
+    digitalWrite(Motor2Dir1, LOW);
+    delay(1);
+    analogWrite(Motor2Dir0, MixedSpeeds[0]);
+  } else {
+    digitalWrite(Motor2Dir0, LOW);
+    delay(1);
+    analogWrite(Motor2Dir1, MixedSpeeds[0]);
+  }
+}
+
+void motorMixer(int fwd, int trn) {
+  int speeds[2];
+
+  if ((fwd == 0) || (trn == 0)) {
+    speeds[0] = constrain((fwd + trn), -255, 255);
+    speeds[1] = constrain((fwd - trn), -255, 255);
+  } else if (trn > 0) {
+    if (fwd > 0) {
+      speeds[0] = max(abs(fwd), abs(trn));
+      speeds[1] = 255 - (trn / 2) - ((255 - fwd) / 2);
+    } else {
+      speeds[0] = -1 * max(abs(fwd), abs(trn));
+      speeds[1] = -255 + (trn / 2) + ((255 + fwd) / 2);
+    }
+  } else {
+    if (fwd > 0) {
+      speeds[0] = 255 - (trn / 2) - ((255 - fwd) / 2);
+      speeds[1] = max(abs(fwd), abs(trn));
+    } else {
+      speeds[0] = -255 + (trn / 2) + ((255 + fwd) / 2);
+      speeds[1] = -1 * max(abs(fwd), abs(trn));
+    }
   }
 
-  //needs to be tuned
-  delay(100);
-  //end needs to be tuned
+  MixedSpeeds[0] = speeds[0];
+  MixedSpeeds[1] = speeds[1];
 }
-
-void updateMotors() {
-  motorValues[0] = ch[0];
-  motorValues[1] = ch[3];
-  int linearValue = map(ch[1], 0, 1000, 0, 1023);
-  int rotationalValue = map(ch[2], 0, 1000, 0, 1023);
-
-  //need to finish mixing code
-}
-
-
 
 
 
@@ -79,9 +140,19 @@ void read_me()  {
     }
     i=0;
   }
-}//copy store all values from temporary array another array after 15 reading  
+}//copy store all values from temporary array another array after 15 reading 
 
-void read_rc(){
+void computeAVG() {
+  for (int i = 0; i < ChannelsUsed; i++) {
+    int sum = 0;
+    for (int index = 0; index < iterations; index++) {
+      sum += ch[index][i];
+    }
+    avg[i] = (sum / iterations);
+  }
+}
+
+void read_rc() {
   int i,j,k=0;
   for(k=14;k>-1;k--){
     if(ch1[k]>10000){
@@ -96,4 +167,5 @@ void read_rc(){
   for(i=0;i < ChannelsUsed;i++){
     ch[iterations-1][i]=(ch1[i+j+1]-1000);
   }
+  computeAVG();
 }     //assign ChannelsUsed channel values after separation space
