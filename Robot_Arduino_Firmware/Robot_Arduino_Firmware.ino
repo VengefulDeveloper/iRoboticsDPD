8dee82a910f10bc6e9c3f9c2784677185c4d5fec
VengefulDeveloper <el114179@gmail.com>
2017-09-17 21:02:03 -0500

    Motor Mixer Finished
    Removed Servo Library and created my own methods to control the weapon's ESC using millis() to create the desired waveform for the corresponding speed. Using the Servo library utilizes the pwm clock used for pins 9 and 10, of which pin 9 is used to control on of the directions of a drive motor. Fixed some issues with residual voltage (~70mV) being on the output of the motors by turning off the enable pin when the inputs are near 0 (within the deadband specified).
    
    Refactored some of the main loop to maximize the loop speed so the "crude" method of using millis() to create the desired digital waveform for the weapon's ESC signal (so there is no delay between when the signal should go low, and when the loop reaches to code to set it low).

--- Robot_Arduino_Firmware/Robot_Arduino_Firmware.ino
+++ Robot_Arduino_Firmware/Robot_Arduino_Firmware.ino
@@ -1,5 +1,3 @@
-#include <Servo.h>
-
 //original ppm code by abhilash_patel
 
 #define ChannelsUsed 4
@@ -12,6 +10,7 @@ int avg[ChannelsUsed];
 int MixedSpeeds[2] = {0, 0};
 int motorValues[2] = {1, 1};
 
+#define motorUpdateTime 20
 #define weaponESC 9
 
 #define Motor1Dir0 10
@@ -24,15 +23,17 @@ int motorValues[2] = {1, 1};
 
 #define ReceiverPin 3
 
-#define motorDeadband 0.03 //percent error between commanded and set motor pwm value
-#define Motor1Multiplier 1  //change to -1 to reverse direction
-#define Motor2Multiplier 1
+#define motorDeadband 0.1 //percent error between commanded and set motor pwm value
 
 #define redLED A0
 #define blueLED A5
 
-Servo ServoWeaponESC;
+bool alreadyRunning = false;
+unsigned long lastTime = 0;
 
+unsigned long escFrameStart = millis();
+unsigned long escPulseStop = millis();
+float escPulseWidth;
 
 void setup() {
   Serial.begin(115200);
@@ -47,31 +48,52 @@ void setup() {
   pinMode(Motor1Enable, OUTPUT);
   pinMode(Motor2Enable, OUTPUT);
 
-  pinMode(A0, OUTPUT);
-  pinMode(A5, OUTPUT);
-
-  ServoWeaponESC.attach(weaponESC);
+  pinMode(redLED, OUTPUT);
+  pinMode(blueLED, OUTPUT);
+  
+  digitalWrite(blueLED, LOW);
+  digitalWrite(redLED, HIGH);
 }
 
 void loop() {
   read_rc();
+
+  updateWeaponESC();
+
   if (ch[iterations-1][3] > 700) {
-    digitalWrite(blueLED, HIGH);
-    digitalWrite(redLED, LOW);
-    enableDrive();
-    ServoWeaponESC.write(map(ch[iterations][1], 0, 1000, 0, 180));
+    if (!alreadyRunning) {
+      digitalWrite(blueLED, HIGH);
+      digitalWrite(redLED, LOW);
+      enableDrive();
+      alreadyRunning = true;
+    }
     
-    if ( 
-      (abs( ((motorValues[0] - avg[2]) / float(motorValues[0]))) > motorDeadband) ||
-      (abs( ((motorValues[1] - avg[0]) / float(motorValues[1]))) > motorDeadband) 
-      ) {
+    updateWeaponESC();
+    Serial.println("FWD: " + String(avg[2]) + ";\tTRN: " + String(avg[0]));
+
+    if (motorUpdateTime < millis() - lastTime) {
+      if ( (abs( ((motorValues[0] - avg[2]) / float(motorValues[0]))) > motorDeadband) || (abs(((motorValues[1] - avg[0]) / float(motorValues[1]))) > motorDeadband) ) {
+        lastTime = millis();
         updateMotors(avg[2], avg[0]);
       }
-      delay(25);
+    }
   } else {
-    //NO RADIO CONNECTED
-    digitalWrite(blueLED, LOW);
-    digitalWrite(redLED, HIGH);
+    if (alreadyRunning) {
+      digitalWrite(blueLED, LOW);
+      digitalWrite(redLED, HIGH);
+      alreadyRunning = false;
+      disableDrive();
+    }
+  }
+}
+
+void updateWeaponESC() {
+  if (millis() >= escFrameStart + 20) {
+    escPulseWidth = (float(map(ch[iterations-1][1], 0,1000, 1005, 1995))/1000.0);
+    digitalWrite(weaponESC, HIGH);
+    escPulseStop = millis() + escPulseWidth;
+  } else  if (millis() >= escPulseStop) {
+    digitalWrite(weaponESC, LOW);
   }
 }
 
@@ -80,38 +102,50 @@ void enableDrive() {
   digitalWrite(Motor2Enable, HIGH);
 }
 
+void disableDrive() {
+  digitalWrite(Motor1Enable, LOW);
+  digitalWrite(Motor2Enable, LOW);
+}
+
 void updateMotors(int Ch1, int Ch2) {
   motorValues[0] = Ch1;
   motorValues[1] = Ch2;
   int linearValue = map(Ch1, 0, 1000, -255, 255);
   int rotationalValue = map(Ch2, 0, 1000, -255, 255);
 
+  if (abs(linearValue) < motorDeadband*255) {
+    linearValue = 0;
+  }
+  if (abs(rotationalValue) < motorDeadband*255) {
+    rotationalValue = 0;
+  }
+
   motorMixer(linearValue, rotationalValue);
-  Serial.println("M0: " + String(MixedSpeeds[0]) + ";\tM1: " + String(MixedSpeeds[1]));
+  
   if (MixedSpeeds[0] > 0) {
     digitalWrite(Motor1Dir1, LOW);
-    delay(1);
     analogWrite(Motor1Dir0, abs(MixedSpeeds[0]));
+
   } else {
     digitalWrite(Motor1Dir0, LOW);
-    delay(1);
     analogWrite(Motor1Dir1, abs(MixedSpeeds[0]));
+
   }
   if (MixedSpeeds[1] > 0) {
     digitalWrite(Motor2Dir1, LOW);
-    delay(1);
     analogWrite(Motor2Dir0, abs(MixedSpeeds[1]));
+
   } else {
     digitalWrite(Motor2Dir0, LOW);
-    delay(1);
     analogWrite(Motor2Dir1, abs(MixedSpeeds[1]));
+
   }
+
 }
 
 void motorMixer(int fwd, int trn) {
   int speeds[2];
-  if ( ( (fwd  < (255 * motorDeadband)) && (fwd > (-255 * motorDeadband))) || ((trn  < (255 * motorDeadband)) && (trn > (-255 * motorDeadband)))) {  //if axis is zero
-
+  if ( ( (fwd  < (255 * motorDeadband)) && (fwd > (-255 * motorDeadband))) || ((trn  < (255 * motorDeadband)) && (trn > (-255 * motorDeadband)))) {  //if either axis is zero
     speeds[0] = constrain((fwd + trn), -255, 255);
     speeds[1] = constrain((fwd - trn), -255, 255);
   } else if (trn > 0) {
@@ -135,7 +169,17 @@ void motorMixer(int fwd, int trn) {
       speeds[1] = -1 * max(abs(fwd), abs(trn));
     }
   }
+  if (speeds[0] == 0) {
+    digitalWrite(Motor1Enable, LOW);
+  } else {
+    digitalWrite(Motor1Enable, HIGH);
+  }
 
+  if (speeds[1] == 0) {
+    digitalWrite(Motor2Enable, LOW);
+  } else {
+    digitalWrite(Motor2Enable, HIGH);
+  }
   MixedSpeeds[0] = constrain(speeds[0],-255,255);
   MixedSpeeds[1] = constrain(speeds[1],-255,255);
 }
